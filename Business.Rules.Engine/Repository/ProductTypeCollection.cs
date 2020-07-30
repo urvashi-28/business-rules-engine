43fc52d3547624e9db1d4d34d01dbc4994509e50
Urvashi Gupta <urvashi.28@outlook.com>
2020-07-30 08:47:53 +0530

    added Product Type Collection Class

---
+++ Business.Rules.Engine/Repository/ProductTypeCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Rules.Engine.Repository
+{
+    public class ProductTypeCollection
+    {
+        Dictionary<int, string> productType = new Dictionary<int, string>
+        {
+            {1, "PhysicalProduct"},
+            {2, "PhysicalProduct"},
+            {3, "Book"},
+            {4, "Book"},
+            {5, "ActivateMembership"},
+            {6, "UpgradeMembership"},
+            {7, "Video"},
+            {8, "Video"}
+        };
+
+        public string GetProductType(int productId)
+        {
+            return productType[productId];
+        }        
+    }
+}
---
+++ Business.Rules.Enigine.UnitTests/Repository/ProductTypeCollectionShould.cs
@@ -0,0 +1,34 @@
+﻿using Business.Rules.Engine.Repository;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Rules.Enigine.UnitTests.Repository
+{
+    [TestFixture]
+    public class ProductTypeCollectionShould
+    {
+        private ProductTypeCollection productTypeCollection;
+
+        [SetUp]
+        public void Setup()
+        {
+            productTypeCollection = new ProductTypeCollection();
+        }
+
+        [TestCase(1, "PhysicalProduct")]
+        [TestCase(3, "Book")]
+        [TestCase(6, "UpgradeMembership")]
+        [Test]
+        public void ReturnExpetedProductTypeWhenProductIdPassed(int productId, string expectedProductType)
+        {
+            var actualProductType = productTypeCollection.GetProductType(productId);
+
+            actualProductType.Should().Be(expectedProductType);
+        }
+    }
+}
