cb25d6736dfa8fc4fa573869b32ebfe05b0ad5f3
Urvashi Gupta <urvashi.28@outlook.com>
2020-07-30 11:42:46 +0530

    Integrated membership action to membership processor

--- Business.Rules.Engine/Processors/MembershipProcessor.cs
+++ Business.Rules.Engine/Processors/MembershipProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Business.Rules.Engine.ProductAction;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,41 @@ namespace Business.Rules.Engine.Processors
 
     public class MembershipProcessor : IMembershipProcessor
     {
+        private IMembershipActions _membershipActions;
+
+        public MembershipProcessor(IMembershipActions membershipActions)
+        {
+            _membershipActions = membershipActions;
+        }
+
         public void ActivateMembership()
         {
-            
+            try
+            {
+                var membershipResult = _membershipActions.ActivateMembership() ? "Membership Activated" : "Error in activating membership";
+
+                Console.WriteLine(membershipResult);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
         }
 
         public void UpgradeMembership()
         {
-            
+            try
+            {
+                var membershipResult = _membershipActions.UpgradeMembership() ? "Membership Upgraded" : "Error in upgradation membership";
+
+                Console.WriteLine(membershipResult);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
         }
     }
 }
--- Business.Rules.Enigine.UnitTests/Processors/MembershipProcessorShould.cs
+++ Business.Rules.Enigine.UnitTests/Processors/MembershipProcessorShould.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Rules.Engine.Processors;
+using Business.Rules.Engine.ProductAction;
+using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -12,11 +14,53 @@ namespace Business.Rules.Enigine.UnitTests.Processors
     public class MembershipProcessorShould
     {
         private IMembershipProcessor _membershipProcessor;
+        private Mock<IMembershipActions> _mockMembershipActions;
 
         [SetUp]
         public void Setup()
         {
-            _membershipProcessor = new MembershipProcessor();
+            _mockMembershipActions = new Mock<IMembershipActions>();
+            _membershipProcessor = new MembershipProcessor(_mockMembershipActions.Object);
+        }
+
+        [Test]
+        public void ProcessActivateMembershipWhenActivateMembershipCalledAndMembershipActionReturendTrue()
+        {
+            _mockMembershipActions.Setup(m => m.ActivateMembership()).Returns(true);
+
+            _membershipProcessor.ActivateMembership();
+
+            _mockMembershipActions.Verify(m => m.ActivateMembership(), Times.Once);
+        }
+
+        [Test]
+        public void ProcessUpgradeMembershipWhenUpgradeMembershipCalledAndMembershipActionReturendTrue()
+        {
+            _mockMembershipActions.Setup(m => m.UpgradeMembership()).Returns(true);
+
+            _membershipProcessor.UpgradeMembership();
+
+            _mockMembershipActions.Verify(m => m.UpgradeMembership(), Times.Once);
+        }
+
+        [Test]
+        public void ThrowTimeoutExceptionIfMembershipActionThrowsTimeoutExceptionWhenActivateMembershipCalled()
+        {
+            _mockMembershipActions.Setup(m => m.ActivateMembership()).Throws<TimeoutException>();
+
+            Assert.Throws<TimeoutException>(() => _membershipProcessor.ActivateMembership());
+
+            _mockMembershipActions.Verify(m => m.ActivateMembership(), Times.Once);
+        }
+
+        [Test]
+        public void ThrowTimeoutExceptionIfMembershipActionThrowsTimeoutExceptionWhenUpgradeCalled()
+        {
+            _mockMembershipActions.Setup(m => m.UpgradeMembership()).Throws<TimeoutException>();
+
+            Assert.Throws<TimeoutException>(() => _membershipProcessor.UpgradeMembership());
+
+            _mockMembershipActions.Verify(m => m.UpgradeMembership(), Times.Once);
         }
     }
 }
