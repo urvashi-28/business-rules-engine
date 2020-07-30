26965c224ceb880219528c258a9ad3a4ce16543e
Urvashi Gupta <urvashi.28@outlook.com>
2020-07-30 11:18:01 +0530

    Added Membership action class

---
+++ Business.Rules.Engine/ProductAction/MembershipActions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Rules.Engine.ProductAction
+{
+    public interface IMembershipActions
+    {
+        bool ActivateMembership();
+
+        bool UpgradeMembership();
+    }
+
+    public class MembershipActions : IMembershipActions
+    {
+        public bool ActivateMembership()
+        {
+            // oprations to activate memberships. If operation is successful return true else false.
+            try
+            {
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public bool UpgradeMembership()
+        {
+            // oprations to activate memberships. If operation is successful return true else false.
+            try
+            {
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ Business.Rules.Enigine.UnitTests/ProductAction/MembershipActionsShould.cs
@@ -0,0 +1,39 @@
+﻿using Business.Rules.Engine.ProductAction;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Rules.Enigine.UnitTests.ProductAction
+{
+    [TestFixture]
+    public class MembershipActionsShould
+    {
+        private IMembershipActions _membershipActions;
+
+        [SetUp]
+        public void Setup()
+        {
+            _membershipActions = new MembershipActions();
+        }
+
+        [Test]
+        public void ActivateMembershipWhenActivateMembershipCalled()
+        {
+            var result = _membershipActions.ActivateMembership();
+
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void UpgradeMembershipWhenUpgradeMembershipCalled()
+        {
+            var result = _membershipActions.UpgradeMembership();
+
+            result.Should().BeTrue();
+        }
+    }
+}
